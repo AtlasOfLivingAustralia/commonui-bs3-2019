cd482a942402cfc921d6011ed74856a36d170d0e
Matt Andrews <Matt.Andrews@csiro.au>
2023-08-21 16:10:23 +1000

    buildvars examples

---
+++ buildvars-ignored/buildvars-prod.js
@@ -0,0 +1,8 @@
+// buildvars.js is git-ignored and has to be created locally for each gulp build environment.
+// Variables to use when substituting tokens during gulp build:
+module.exports = {
+    homeDomain: "https://www.ala.org.au",
+    signUpURL: "https://auth.ala.org.au/userdetails/registration/createAccount",
+    profileURL: "https://auth.ala.org.au/userdetails/myprofile",
+    fathomID: "QDWZORBB"
+};
